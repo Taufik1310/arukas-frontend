2170e3fe9e023e13b6f6939d190b1793564c3ae6
Taufik1310 <taufikupik1310@gmail.com>
2026-07-01 13:19:26 +0700

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      { protocol: "http", hostname: "localhost" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+    ],
+  },
+  webpack: (config) => {
+    // Wajib untuk quagga barcode scanner
+    config.externals = [...(config.externals || []), "canvas", "jsdom"];
+    return config;
+  },
 };
 
 export default nextConfig;
